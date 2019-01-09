be26b819343adffec580488a5102bfc59c3a23d9
donaldRwilliams <drwwilliams@ucdavis.edu>
2019-01-09 11:12:42 -0800

    predictive plot documentation

--- R/predictive_plot.R
+++ R/predictive_plot.R
@@ -1,13 +1,28 @@
 #' Title
 #'
-#' @param x
-#' @param size
-#' @param color
+#' @param x object of class \code{bayes_R2} or \code{predictive error}
+#' @param size size of the points
+#' @param color color of points
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#'
+#' # X is the data matrix of dimensions n by p
+#' fit <- bayes_estimate(X)
+#'
+#' # select the graphical structure
+#' select <- estimate_select(fit, ci_width = .95)
+#'
+#' # compute Bayesian R2
+#' r2 <-  bayes_R2(fit, selected = select$adjacency_mat, ci_width = .95, samples = 1000)
+#'
+#' plt <- predictive_plot(x = r2, size = 3, color = color)
+#'
+#' # plot can be changed further with ggplot--e.g., changing theme
+#'
+#' plt + theme_classic()
 predictive_plot <- function(x, size, color){
 
   if(class(x) == "bayes_R2"){
