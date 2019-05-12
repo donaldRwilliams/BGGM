1bd919ddaa8884f256983dfd44aa6a85387e1a86
donaldRwilliams <drwwilliams@ucdavis.edu>
2019-05-12 07:56:04 -0700

    removed old functions

--- R/helpers.R
+++ R/helpers.R
@@ -15,6 +15,30 @@ get_lower_tri<-function(cormat){
 }
 
 
+beta_summary <- function(x, node, ci_width, samples){
+
+  # convert inverse to beta
+  x <- BGGM:::inverse_2_beta(x, samples = samples)
+
+  # stop if not the correct class
+  if(class(x) != "inverse_2_beta"){
+    stop("class must be inverse_2_beta")
+  }
+
+  # check ci_width is allowed
+  if(ci_width >= 1 | ci_width <= 0){
+    stop("ci_width must be between 0 and 1")
+  }
+  returned_object <- lapply(node, function(y) BGGM:::summary_beta_helper(node =  y,
+                                                                         x = x,
+                                                                         ci_width))
+  class(returned_object) <- "beta_summary"
+
+  returned_object$call <- match.call()
+  returned_object
+}
+
+
 
 net_plot <- function(x, layout = "circle", mat_type, node_outer, node_inner, node_text_size){
 
