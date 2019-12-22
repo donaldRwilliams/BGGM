2105aabcc2d6e1d16756c496a41438378cb5b316
donaldRwilliams <drwwilliams@ucdavis.edu>
2019-12-21 17:28:41 -0800

    fixed vignette !

--- doc/ppc1.R
+++ doc/ppc1.R
@@ -1,43 +1,30 @@
 params <-
 list(EVAL = FALSE)
 
-## ----include=FALSE----------------------------------------------------------------------
-stopifnot(require(knitr))
-options(width = 90)
-opts_chunk$set(
-  comment = NA,
-  message = FALSE,
-  warning = FALSE,
-  eval = if (isTRUE(exists("params"))) params$EVAL else FALSE,
-  dev = "png",
-  dpi = 150,
-  fig.asp = 0.8,
-  fig.width = 5,
-  out.width = "60%",
-  fig.align = "center"
-  )
-library(BGGM)
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>", fig.width = 7, fig.height = 7, fig.align = "center")
 library(ggplot2)
+library(BGGM)
 
-## ---------------------------------------------------------------------------------------
-#  dat <- BGGM::bfi
-#  
-#  dat_males <- subset(dat, gender == 1)[,1:25]
-#  
-#  dat_female <- subset(dat, gender == 2)[,1:25]
-#  
-#  # fit model
-#  fit1 <- ggm_compare_ppc(dat_males,
-#                          dat_female,
-#                          iter = 500,
-#                          cores = 4)
-#  
+## -----------------------------------------------------------------------------
+dat <- BGGM::bfi 
 
-## ----fit1-------------------------------------------------------------------------------
-#  summary(fit1)
+dat_males <- subset(dat, gender == 1)[,1:25]
 
-## ---------------------------------------------------------------------------------------
-#  plot(fit1, critical = 0.05) +
-#    theme_bw() +
-#    theme(legend.position = "none")
+dat_female <- subset(dat, gender == 2)[,1:25]
+
+# fit model
+fit1 <- ggm_compare_ppc(dat_males, 
+                        dat_female, 
+                        iter = 500, 
+                        cores = 4)
+
+
+## ----fit1---------------------------------------------------------------------
+summary(fit1)
+
+## -----------------------------------------------------------------------------
+plot(fit1, critical = 0.05) + 
+  theme_bw() + 
+  theme(legend.position = "none")
 
