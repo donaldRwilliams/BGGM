68de9c47756e1f295dc6c49f0f198aefa68ceb84
Donald R. Williams <brycewayne03.29@gmail.com>
2020-12-22 13:20:07 -0800

    add posterior predict and predicted probabilities

---
+++ R/posterior_predict.R
@@ -0,0 +1,73 @@
+#' Draw Samples from the Posterior Predictive Distribution
+#'
+#' @param object An object of class \code{estimate} or \code{explore}
+#'
+#' @param iter Numeric. Number of samples from the predictive distribution
+#'
+#' @param progress Logical. Should a progress bar be included (defaults to \code{TRUE})
+#'
+#' @return A 3D array containing the predicted datasets
+#'
+#' @note Currently only implemented for \code{type = "mixed"}. Note the term mixed is confusing, in that
+#' it can be used with only, say, ordinal data. In this case, re-estimate the model with
+#' \code{type = "mixed"}
+#' until all data types are supported.
+#'
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' Y <- gss
+#'
+#' fit <- estimate(as.matrix(Y),
+#'                 impute = TRUE,
+#'                iter = 150, type = "mixed")
+#'
+#' yrep <- posterior_predict(fit, iter = 100)
+#' }
+posterior_predict <- function(object,
+                              iter = 1000,
+                              progress = TRUE){
+
+
+  if(!any(class(object) %in% c("estimate", "explore"))) {
+    stop("object must be of class 'estimate' or 'explore'.")
+  }
+  if(object$type != "mixed"){
+    stop("type must be 'mixed'.")
+  }
+  Y <- object$Y
+  cors <- pcor_to_cor(object, iter = iter + 1)
+  cors <- cors$R[,,-1]
+
+  n <- object$n
+  p <- object$p
+
+  predicted <- array(0, c(n, p, iter))
+
+
+  if(isTRUE(progress)){
+    pb <- utils::txtProgressBar(min = 0, max = iter, style = 3)
+  }
+
+  for(s in 1:iter){
+    cors_s <- cors[,,s]
+    ypred_s <- mvnrnd(object$n, rep(0, p), cors_s)
+
+    predicted[,,s] <- sapply(1:p, function(j) {
+      quantile(na.omit(Y[, j]), pnorm(ypred_s[, j], 0,
+                                      sqrt(cors_s[j, j])),
+               na.rm = TRUE, type = 1)
+    }
+
+    )
+
+
+    if(isTRUE(progress)){
+      utils::setTxtProgressBar(pb, s)
+    }
+  }
+  dimnames(predicted)[[2]]  <- colnames(Y)
+  class(predicted) <- c("array", "posterior_predict")
+  return(predicted)
+}
---
+++ R/pred_prob.R
@@ -0,0 +1,90 @@
+#' Predicted Probabilities
+#'
+#' Compute predicted probabilities for discrete data, with the possibility
+#' of conditional probabilities (i.e., at fixed values of other nodes)
+#'
+#' @param object An object of class \code{posterior_predict}
+#'
+#' @param outcome Character string. Node for which the probabilities are computed.
+#'
+#' @param Y Matrix (or data frame) of dimensions \emph{n} (observations) by  \emph{p} (variables).
+#'        This must include the column names.
+#'
+#' @param ... Compute conditional probabilities by specifying a column name in \code{Y}
+#'            (besides the \code{outcome}) and a fixed value. This can include
+#'            any number of nodes. See example below..
+#'
+#' @return A list containing a matrix with the computed probabilities
+#'        (a row for each predictive sample and a column for each category).
+#'
+#'
+#' @note There are no checks that the conditional probability exists, i.e., suppose
+#'       you wish to condition on, say, B3 = 2 and B4 = 1, yet there is no instance in
+#'       which B3 is 2 AND B4 is 1. This will result in an uninformative error.
+#'
+#' @export
+#'
+#' @examples
+#' \dontest{
+#' Y <- ptsd
+#' fit <- estimate(as.matrix(Y), iter = 150, type = "mixed")
+#'
+#' pred <- posterior_predict(fit, iter = 100)
+#'
+#' prob <- predicted_probability(pred,
+#'                               Y = Y,
+#'                               outcome = "B3",
+#'                               B4 = 0,
+#'                               B5 = 0)
+#'
+#' }
+predicted_probability <- function(object,  outcome, Y, ...){
+
+  # note: test is for checking the subsetting of the 3d arrays
+  unique_values <- sort(unique(Y[,outcome]))
+  K <- length(unique_values)
+  iter <- dim(object)[3]
+  collect <- matrix(0, iter, K)
+
+  if(!is(object, "posterior_predict")){
+    stop("must be of class 'posterior_predict'")
+  }
+
+  dots <- list(...)
+  test <- list()
+
+  if(length(dots) == 0){
+
+    for(i in 1:iter){
+      collect[i,] <- sapply(1:K, function(x) sum(object[,outcome,i] == unique_values[x])  )
+    }
+
+
+
+  }  else {
+    text_eval <- sapply(1:length(dots), function(x) {
+      paste("sub_set[,", x, "] == ", dots[[x]])
+
+    })
+
+    for(i in 1:iter){
+
+      sub_set <- as.matrix(object[,names(dots),i])
+
+      conditional <- eval(parse(text =
+                                  paste("object[which(", paste(unlist(text_eval),
+                                                               collapse = "  & "), "),,", i,"]")
+      ))
+
+      test[[i]] <- conditional
+
+      collect[i, ] <- sapply(1:K, function(x) sum(conditional[,outcome]
+                                                  == unique_values[x]))
+    }
+  }
+  collect <- t(apply(collect, 1,function(x){x / sum(x)}))
+  colnames(collect) <-  unique_values
+  returned_object <- list(collect = collect, sub_sets = test, dots = dots)
+  return(returned_object)
+
+}
