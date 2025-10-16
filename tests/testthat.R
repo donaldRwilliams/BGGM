2d220ace76100fa5b6e4206e8d2851286d31688a
Philippe Rast <rast.ph@gmail.com>
2025-10-15 17:26:45 -0700

    Stray letter in testthat.R removed

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
-t# This file is part of the standard setup for testthat.
+# This file is part of the standard setup for testthat.
 # It is recommended that you do not modify it.
 #
 # Where should you do additional test configuration?
